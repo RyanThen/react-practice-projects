b1f850b974ffd08d814de9dc2288af8e85b45f26
Ryan <39573308+RyanThen@users.noreply.github.com>
2023-05-01 11:45:13 -0500

    added delete and edit functionality

--- reading-list-app/src/App.js
+++ reading-list-app/src/App.js
@@ -5,6 +5,13 @@ import BookList from './components/BookList';
 function App() {
   const [books, setBooks] = useState([]);
 
+  const deleteBookById = function(bookID) {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== bookID;
+    });
+    setBooks(updatedBooks);
+  }
+
   const createBook = (title) => {
     const updatedBooks = [
       ...books,
@@ -12,13 +19,13 @@ function App() {
         id: Math.round(Math.random() * 99999),
         title 
       },
-    ]
+    ];
     setBooks(updatedBooks);
   }
 
   return (
     <div className="app">
-      <BookList books={books} />
+      <BookList books={books} onDelete={deleteBookById} />
       <BookCreate onSubmit={createBook} />
     </div>
   )
--- reading-list-app/src/components/BookShow.js
+++ reading-list-app/src/components/BookShow.js
@@ -1,5 +1,29 @@
-function BookShow({ book }) {
-  return <div className="book-show">{ book.title }</div>
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+
+function BookShow({ book, onDelete }) {
+  const [bookEdit, setBookEdit] = useState(false);
+
+  const handleEditClick = () => {
+    setBookEdit(!bookEdit);
+  }
+
+  const handleDeleteClick = () => {
+    onDelete(book.id);
+  }
+
+  let content = <h3>{ book.title }</h3>;
+  if(bookEdit) content = <BookEdit />
+
+  return (
+    <div className="book-show">
+      <div>{ content }</div>
+      <div className="actions">
+        <button className="edit" onClick={handleEditClick}>Edit</button>
+        <button className="delete" onClick={handleDeleteClick}>Delete</button>
+      </div>
+    </div>
+  )
 }
 
 export default BookShow;
