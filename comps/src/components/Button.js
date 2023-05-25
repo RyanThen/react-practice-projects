0b58010560b9ab95e3d35179c2dea06ed8ce2bbd
Ryan <39573308+RyanThen@users.noreply.github.com>
2023-05-24 19:43:22 -0500

    button component

--- comps/src/components/Button.js
+++ comps/src/components/Button.js
@@ -1,9 +1,26 @@
-function Button({ children }) {
+import PropTypes from 'prop-types';
+
+function Button({ 
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded
+ }) {
   return (
     <div className="btn-container">
       <button>{children}</button>
     </div>
   )
+}
+
+Button.propTypes = {
+  checkVariationValue: (props) => {
+    return new Error('Invalid props');
+  }
 }
 
 export default Button;
