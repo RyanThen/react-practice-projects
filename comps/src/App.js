2b5268c0d1a3fa770dcff582815362b2f16be48b
Ryan <39573308+RyanThen@users.noreply.github.com>
2023-06-04 19:23:03 -0500

    progress on Accordion component

--- comps/src/App.js
+++ comps/src/App.js
@@ -5,15 +5,18 @@ function App() {
   const items = [
     {
       label: 'Can I use React on any project?',
-      content: 'Lorem ipsum dolor set idum loret issum. Lorem ipsum dolor set idum loret issum.'
+      content: 'Lorem ipsum dolor set idum loret issum. Lorem ipsum dolor set idum loret issum.',
+      id: 1
     },
     {
       label: 'When is React a good choice?',
-      content: 'Lorem ipsum dolor set idum loret issum. Lorem ipsum dolor set idum loret issum.'
+      content: 'Lorem ipsum dolor set idum loret issum. Lorem ipsum dolor set idum loret issum.',
+      id: 2
     },
     {
       label: 'How popular is React?',
-      content: 'Lorem ipsum dolor set idum loret issum. Lorem ipsum dolor set idum loret issum.'
+      content: 'Lorem ipsum dolor set idum loret issum. Lorem ipsum dolor set idum loret issum.',
+      id: 3
     }
   ]
 
--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -1,5 +1,14 @@
-const Accordion = ({items}) => {
-  return <div>hello</div>
+const Accordion = ({ items }) => {
+  const renderedItems = items.map((item) => {
+    return (
+      <div key={item.id}>
+        <div>{item.label}</div>
+        <div>{item.content}</div>
+      </div>
+    )
+  })
+
+  return <div>{renderedItems}</div>
 }
 
 export default Accordion;
